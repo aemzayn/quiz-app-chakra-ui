6415b3b86ac1d0a06ced528282b0e02f30dc8769
aemzayn <ahmad.muslih22@gmail.com>
2021-06-12 19:02:19 +0700

    move constants to separate file

---
+++ src/libs/constants.ts
@@ -0,0 +1,40 @@
+export const CATEGORY = [
+  { label: "Any", value: "" },
+  { label: "General Knowledge", value: "9" },
+  { label: "Books", value: "10" },
+  { label: "Film", value: "11" },
+  { label: "Music", value: "12" },
+  { label: "Musical Theatres", value: "13" },
+  { label: "Television", value: "14" },
+  { label: "Video games", value: "15" },
+  { label: "Board games", value: "16" },
+  { label: "Science and Nature", value: "17" },
+  { label: "Computer Science", value: "18" },
+  { label: "Math", value: "19" },
+  { label: "Mythology", value: "20" },
+  { label: "Sports", value: "21" },
+  { label: "Geography", value: "22" },
+  { label: "History", value: "23" },
+  { label: "Politics", value: "24" },
+  { label: "Art", value: "25" },
+  { label: "Celebrities", value: "26" },
+  { label: "Animal", value: "27" },
+  { label: "Vehicles", value: "28" },
+  { label: "Comics", value: "29" },
+  { label: "Gadgets", value: "30" },
+  { label: "Anime and Manga", value: "31" },
+  { label: "Cartoons", value: "32" },
+];
+
+export const DIFFICULTY = [
+  { label: "Any", value: "" },
+  { label: "Easy", value: "easy" },
+  { label: "Medium", value: "medium" },
+  { label: "Hard", value: "hard" },
+];
+
+export const TYPE = [
+  { label: "Any", value: "" },
+  { label: "True or false", value: "boolean" },
+  { label: "Multiple choice", value: "multiple" },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,47 +15,7 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import { GoLogoGithub } from "react-icons/go";
-
-const CATEGORY = [
-  { label: "Any", value: "" },
-  { label: "General Knowledge", value: "9" },
-  { label: "Books", value: "10" },
-  { label: "Film", value: "11" },
-  { label: "Music", value: "12" },
-  { label: "Musical Theatres", value: "13" },
-  { label: "Television", value: "14" },
-  { label: "Video games", value: "15" },
-  { label: "Board games", value: "16" },
-  { label: "Science and Nature", value: "17" },
-  { label: "Computer Science", value: "18" },
-  { label: "Math", value: "19" },
-  { label: "Mythology", value: "20" },
-  { label: "Sports", value: "21" },
-  { label: "Geography", value: "22" },
-  { label: "History", value: "23" },
-  { label: "Politics", value: "24" },
-  { label: "Art", value: "25" },
-  { label: "Celebrities", value: "26" },
-  { label: "Animal", value: "27" },
-  { label: "Vehicles", value: "28" },
-  { label: "Comics", value: "29" },
-  { label: "Gadgets", value: "30" },
-  { label: "Anime and Manga", value: "31" },
-  { label: "Cartoons", value: "32" },
-];
-
-const DIFFICULTY = [
-  { label: "Any", value: "" },
-  { label: "Easy", value: "easy" },
-  { label: "Medium", value: "medium" },
-  { label: "Hard", value: "hard" },
-];
-
-const TYPE = [
-  { label: "Any", value: "" },
-  { label: "True or false", value: "boolean" },
-  { label: "Multiple choice", value: "multiple" },
-];
+import { CATEGORY, DIFFICULTY, TYPE } from "../libs/constants";
 
 const Index = () => {
   const [isLoading, setIsLoading] = useState(false);
